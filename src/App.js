0d44fea57918732ec40a8fb77784a734b41d420a
Josh Woodcock <josh@j0w0.com>
2020-11-21 15:33:22 -0600

    Add BrowserRouter for pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
+
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
+import Checkout from './pages/Checkout/Checkout';
 
 function App() {
   return (
     // BEM css convention
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route exact path='/'><Home /></Route>
+          <Route path='/checkout'><Checkout /></Route>
+          <Route path='*'><Redirect to='/' /></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
