f9248021b765e9e78dd39046fd4ccc0bca19be76
Josh Woodcock <josh@j0w0.com>
2020-11-21 14:20:26 -0600

    Start project - header and home components

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <div className="home__container">
+
+        <img
+          className='home__image'
+          src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2020/Other/RB-3154_SVOD_DiamondFallBack_Nov/Amazon_GW_DesktopTallHero_RB-3154_SVOD_DiamondFallBack_Nov_1500x600._CB418721851_.jpg"
+          alt=""
+        />
+
+        <div className="home__row">
+          {/* <Product /> */}
+          {/* <Product /> */}
+        </div>
+
+        <div className="home__row">
+          {/* <Product /> */}
+          {/* <Product /> */}
+          {/* <Product /> */}
+        </div>
+
+        <div className="home__row">
+          {/* <Product /> */}
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
