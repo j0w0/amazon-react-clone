cfcdeb364cf0f32c369e73e3335a3fa0aab78ba0
Josh Woodcock <josh@j0w0.com>
2020-11-21 15:24:05 -0600

    Create Product component and fill in placeholder props

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Home.css';
 
+import Product from '../../components/Product/Product';
+
 const Home = () => {
   return (
     <div className='home'>
@@ -13,18 +15,54 @@ const Home = () => {
         />
 
         <div className="home__row">
-          {/* <Product /> */}
-          {/* <Product /> */}
+          <Product
+            id='B006HP7GA4'
+            title='On The Rock Glass with Ice Ball Maker'
+            price={19.99}
+            image='https://images-na.ssl-images-amazon.com/images/I/41f4-ZDz0jL._AC_.jpg'
+            rating={3}
+          />
+          <Product
+            id='B007ZH5LQE'
+            title='Nemat Fragrances - Amber'
+            price={17.49}
+            image='https://images-na.ssl-images-amazon.com/images/I/41RZ5kO3koL.jpg'
+            rating={4}
+          />
         </div>
 
         <div className="home__row">
-          {/* <Product /> */}
-          {/* <Product /> */}
-          {/* <Product /> */}
+          <Product
+            id='B01KJEOCDW'
+            title='LEGO Creator Mighty Dinosaurs 31058 Build It Yourself Dinosaur Set, Create a Pterodactyl, Triceratops and T Rex Toy (174 Pieces)'
+            price={11.99}
+            image='https://images-na.ssl-images-amazon.com/images/I/91DCcqMAlzL._AC_SL1500_.jpg'
+            rating={5}
+          />
+          <Product
+            id='B07JJWYQN7'
+            title='Succulent Plant Pots - 3 Inch Small Ceramic Mandala Planter Containers for Flowers or Cactus with Drainage Hole and Bamboo Tray, Set of 3'
+            price={21.90}
+            image='https://images-na.ssl-images-amazon.com/images/I/71byIq6AULL._AC_SL1500_.jpg'
+            rating={5}
+          />
+          <Product
+            id='B07JC9X8JL'
+            title='Mkono Plant Stand - EXCLUDING Plant Pot, Mid Century Wood Flower Pot Holder Potted Stand Indoor Display Rack Rustic Decor, Up to 10 Inch Planter, Natural'
+            price={23.99}
+            image='https://images-na.ssl-images-amazon.com/images/I/71Bqannpl1L._AC_SL1426_.jpg'
+            rating={4}
+          />
         </div>
 
         <div className="home__row">
-          {/* <Product /> */}
+          <Product
+            id='B07B48CFDR'
+            title='AMOYSTONE 1Pair Teal Agate Bookends 2-3 lbs for Books Small Cut Agate Stone Dyed with Rubber Bumpers'
+            price={27.89}
+            image='https://images-na.ssl-images-amazon.com/images/I/71U7ThNmMrL._AC_SL1500_.jpg'
+            rating={4}
+          />
         </div>
 
       </div>
